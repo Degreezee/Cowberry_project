526b6f191a14907833be4f8aae521f73cd3e5363
Degreezee <76453732+Degreezee@users.noreply.github.com>
2022-04-24 23:01:23 +0300

    Add files via upload

--- Functions/pillow_colors.py
+++ Functions/pillow_colors.py
@@ -1,7 +1,7 @@
 from PIL import Image as IMG
 from PIL import ImageColor as Color
 
-def pillow_hex_to_dec(hexi:str): #перевод из шестнадцатиричной HEX в десятичную систему счисления
+def pillow_hex_to_dec(hexi:str):
 	hexi = hexi.lower()
 	# hexi is in format '#ffffff'
 	dec = 0
@@ -33,18 +33,19 @@ def pillow_hex_to_dec(hexi:str): #перевод из шестнадцатири
 	return dec
 
   
-def pillow_dec_to_hex(number:int): #перевод из десятичной системы счисления в шестнадцатиричную HEX
+def pillow_dec_to_hex(number:int):
 	hexi = hex(number) #hexadecimal in the format 0xffffff
 	hexi = hexi[2:] # removeing '0x'
 		
 	if len(hexi) < 6:
-		missing_size = 6 - len(hexi)
-		hexi = "0" * missing_size + hexi
-		# если для создания цвета не хватает чисел, то добавляются нули
-	hexi = "#" + hexi #Добавление "#" для превращения в цвет Hex
+		missing_size = 6 - len(hexi) # calculating number of '0's to fill in
+		hexi = "0" * missing_size + hexi # str * int is repeating str int num of times
+		# this if fills in blacks: '1' --> '000001' 
+	hexi = "#" + hexi # adding '#' to the start  of hexadecimal color 
+	# so hexadecimal '0x0f55ae' turned into pillow form '#0f55ae'
 	return hexi
 
-def RGB_to_pillow_hex(rgb): #Перевод из RGB в HEX
+def RGB_to_pillow_hex(rgb):
 	hex_color = "{:X}{:X}{:X}".format(rgb[0], rgb[1], rgb[2])
 	pillow_hex_color = "#" + "0"*(6-len(hex_color.lower())) + hex_color.lower()
 	return pillow_hex_color
